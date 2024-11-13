c033c1652c331988697a42077dda18d879664653
tomadre70 <emmanuel.abeke@groupe-esigelec.org>
2024-11-13 17:52:56 +0100

    Création du site web

---
+++ accueil.php
@@ -0,0 +1,15 @@
+<?php
+    $titre_page="Accueil";
+    include 'header.inc.php';
+?>
+    <h1>Bienvenue sur le site de running de l'ESIGELEC</h1>
+    
+    <div class="container content-container">
+      <div class="text-area">
+        <h2>À propos de ESIG'Running</h2>
+        <p>Bienvenue sur notre site dédié aux passionnés de course à pied de l'ESIGELEC ! Rejoignez-nous pour des entraînements, des compétitions, et des événements de running. Notre objectif est de promouvoir le sport, l'entraide, et le dépassement de soi.</p>
+      </div>
+    </div>
+    <a href="./connexion.php"><h1>Aller à la page de connexion</h1></a>
+
+<?php include 'footer.inc.php';
---
+++ connexion.php
@@ -0,0 +1,9 @@
+<?php
+    $titre_page="Connexion";
+    include 'header.inc.php';
+?>
+<h1>Hello</h1>
+<h2>Hi</h2>
+<h5>No</h5>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias dolor voluptate iste consequuntur! Rerum beatae odio repellendus quas consequatur natus blanditiis nam! Inventore eum at ad omnis nesciunt illo nemo.</p>
+<a href="./accueil.php"><h1>Aller à la page d'accueil</h1></a>
