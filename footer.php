5179c149bf44bec62a043b3f6a370829a966991d
lucia <lucia.famy@groupe-esigelec.org>
2024-11-18 22:55:57 +0100

    mise à jour des pages header et footer

---
+++ footer.php
@@ -0,0 +1,62 @@
+<footer class="bg-primary text-white pt-4 pb-3">
+    <div class="container">
+        <div class="row">
+            <!-- Section À propos -->
+            <div class="col-md-3">
+                <h5>À propos de ESIG'RUNNING</h5>
+                <p>ESIG'RUNNING facilite les échanges entre apprentis et tuteurs, pour une expérience enrichie et un suivi personnalisé.</p>
+            </div>
+
+            <!-- Liens utiles -->
+            <div class="col-md-3">
+                <h5>Liens utiles</h5>
+                <ul class="list-unstyled">
+                    <li><a href="#" class="text-white">Accueil</a></li>
+                    <li><a href="#" class="text-white">Nos Services</a></li>
+                </ul>
+            </div>
+
+            <!-- Contact -->
+            <div class="col-md-3">
+                <h5>Contact</h5>
+                <ul class="list-unstyled">
+                    <li><i class="fas fa-map-marker-alt"></i> ESIGELEC, Avenue Galilée, 76800 Saint-Étienne-du-Rouvray, France</li>
+                    <li><i class="fas fa-phone"></i> +33 2 32 91 58 58</li>
+                    <li><i class="fas fa-envelope"></i> contact@esig-running.com</li>
+                </ul>
+            </div>
+
+            <!-- Suivez-nous avec Font Awesome logos -->
+            <div class="col-md-3">
+                <h5>Suivez-nous</h5>
+                <div class="d-flex align-items-center">
+                    <a href="https://www.facebook.com" target="_blank" class="text-white mr-3">
+                        <i class="fab fa-facebook fa-lg"></i>
+                    </a>
+                    <a href="https://twitter.com" target="_blank" class="text-white mr-3">
+                        <i class="fab fa-twitter fa-lg"></i>
+                    </a>
+                    <a href="https://www.linkedin.com" target="_blank" class="text-white mr-3">
+                        <i class="fab fa-linkedin fa-lg"></i>
+                    </a>
+                    <a href="https://www.instagram.com" target="_blank" class="text-white">
+                        <i class="fab fa-instagram fa-lg"></i>
+                    </a>
+                </div>
+            </div>
+        </div>
+
+        <hr class="bg-white mt-4">
+        
+        <!-- Droits d'auteur -->
+        <div class="row">
+            <div class="col text-center">
+                <p class="m-0">&copy; 2024 ESIG'RUNNING. Tous droits réservés.</p>
+                <p>Propulsé par les étudiants de l'ESIGELEC pour améliorer l'apprentissage en entreprise.</p>
+            </div>
+        </div>
+    </div>
+
+    <!-- Lien vers Font Awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+</footer>
